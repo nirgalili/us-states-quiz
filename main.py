22296f93807aff974bb816c0cd90cb1abac0a996
Nir Galili <nirgalili1@gmail.com>
2021-09-01 13:31:03 +0300

    finish

--- main.py
+++ main.py
@@ -3,36 +3,43 @@ import pandas
 from state import State
 from scoreboard import Scoreboard
 
-screen = turtle.Screen()
-screen.title("U.S States Quiz")
-image = "blank_states_img.gif"
-screen.addshape(image)
 
-turtle.shape(image)
+def run_main():
+    screen = turtle.Screen()
+    screen.title("U.S States Quiz")
+    image = "blank_states_img.gif"
+    screen.addshape(image)
 
-state = State()
-scoreboard = Scoreboard()
-scoreboard.write_new_score()
+    turtle.shape(image)
 
-df = pandas.read_csv("50_states.csv")
+    state = State()
+    scoreboard = Scoreboard()
+    scoreboard.write_new_score()
 
-user_guesses_list = []
-game_is_on = True
-while game_is_on:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
-    answer_df = df[df.state == answer_state]
-    answer_x = int(answer_df["x"])
-    answer_y = int(answer_df["y"])
+    df = pandas.read_csv("50_states.csv")
 
-    # print(answer_df)
-    if not answer_df.empty and answer_state not in user_guesses_list:
-        state.write_new_state(answer_x, answer_y, answer_state)
-        scoreboard.change_score()
-        user_guesses_list.append(answer_state)
+    user_guesses_list = []
+    game_is_on = True
+    while game_is_on:
+        answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+        answer_df = df[df.state == answer_state]
 
-    if scoreboard.complete_quiz_check():
-        game_is_on = False
 
-    # print(user_guesses_list)
+        # print(answer_df)
+        if not answer_df.empty and answer_state not in user_guesses_list:
+            answer_x = int(answer_df["x"])
+            answer_y = int(answer_df["y"])
+            state.write_new_state(answer_x, answer_y, answer_state)
+            scoreboard.change_score()
+            user_guesses_list.append(answer_state)
 
-screen.exitonclick()
+        if scoreboard.complete_quiz_check():
+            game_is_on = False
+
+        # print(user_guesses_list)
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    run_main()
