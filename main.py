2f308b0386eb43666a6e74539260cdd5f6447bb5
Nir Galili <nirgalili1@gmail.com>
2021-09-01 14:56:32 +0300

    finish with generation of csv file for missing states

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def run_main():
     scoreboard.write_new_score()
 
     df = pandas.read_csv("50_states.csv")
+    states_list = df.state.to_list()
+    # print(states_list)
 
     user_guesses_list = []
     game_is_on = True
@@ -24,8 +26,9 @@ def run_main():
         answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
         answer_df = df[df.state == answer_state]
 
+        if answer_state == "Exit":
+            break
 
-        # print(answer_df)
         if not answer_df.empty and answer_state not in user_guesses_list:
             answer_x = int(answer_df["x"])
             answer_y = int(answer_df["y"])
@@ -37,6 +40,20 @@ def run_main():
             game_is_on = False
 
         # print(user_guesses_list)
+
+    missing_states = []
+    for state in states_list:
+        if state not in user_guesses_list:
+            missing_states.append(state)
+
+    data_dict = {
+        "State": missing_states
+    }
+
+    data = pandas.DataFrame(data_dict)
+    data.to_csv("missing_states.csv")
+
+
 
     screen.exitonclick()
 
